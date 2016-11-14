a76deeb313e9ed4517547dbd76aa4c3b9678d51e
Xeta <zoro.fmt@gmail.com>
2016-11-14 23:51:35 +0100

    i means... untabify !

--- tests/Network/ServerTest.php
+++ tests/Network/ServerTest.php
@@ -8,43 +8,43 @@ use SkinnyTest\Lib\Utility;
 
 class ServerTest extends TestCase
 {
-	/**
-	 * testConstructNoToken method
-	 *
-	 * @expectedException \RuntimeException
-	 *
-	 * @return void
-	 */
-	public function testConstructNoToken()
-	{
-		$network = new Server();
-	}
+    /**
+     * testConstructNoToken method
+     *
+     * @expectedException \RuntimeException
+     *
+     * @return void
+     */
+    public function testConstructNoToken()
+    {
+        $network = new Server();
+    }
 
-	/**
-	 * testConstruct method
-	 *
-	 * @return void
-	 */
-	public function testConstruct()
-	{
-		Configure::write('Discord.token', 'another token');
+    /**
+     * testConstruct method
+     *
+     * @return void
+     */
+    public function testConstruct()
+    {
+        Configure::write('Discord.token', 'another token');
 
-		$network = new Server();
-		$this->assertInstanceOf('\Discord\Discord', $network->Discord);
-		$this->assertInstanceOf('\Skinny\Module\ModuleManager', $network->ModuleManager);
-	}
+        $network = new Server();
+        $this->assertInstanceOf('\Discord\Discord', $network->Discord);
+        $this->assertInstanceOf('\Skinny\Module\ModuleManager', $network->ModuleManager);
+    }
 
-	/**
-	 * testConstruct method
-	 *
-	 * @return void
-	 */
-	public function testConstructWithModulesPriority()
-	{
-		Configure::write('Discord.token', 'another token');
-		Configure::write('Modules.priority', ['Test']);
+    /**
+     * testConstruct method
+     *
+     * @return void
+     */
+    public function testConstructWithModulesPriority()
+    {
+        Configure::write('Discord.token', 'another token');
+        Configure::write('Modules.priority', ['Test']);
 
-		$network = new Server();
-		$this->assertSame(['Test'], Utility::callProtectedProperty($network->ModuleManager, 'priorityList'));
-	}
+        $network = new Server();
+        $this->assertSame(['Test'], Utility::callProtectedProperty($network->ModuleManager, 'priorityList'));
+    }
 }
