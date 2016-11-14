f3979b73ee4f3fb0b154e7d5b1b7c31d2247874e
Xeta <zoro.fmt@gmail.com>
2016-11-14 23:50:00 +0100

    tabify the file

--- src/Network/Server.php
+++ src/Network/Server.php
@@ -11,91 +11,91 @@ use Skinny\Utility\User;
 
 class Server
 {
-    /**
-     * The Discord instance.
-     *
-     * @var \Discord\Discord
-     */
-    public $Discord;
+	/**
+	 * The Discord instance.
+	 *
+	 * @var \Discord\Discord
+	 */
+	public $Discord;
 
-    /**
-     * The Module Manager instance.
-     *
-     * @var \Skinny\Module\ModuleManager
-     */
-    public $ModuleManager;
+	/**
+	 * The Module Manager instance.
+	 *
+	 * @var \Skinny\Module\ModuleManager
+	 */
+	public $ModuleManager;
 
-    /**
-     * Initialize the Bot and and the ModuleManager.
-     */
-    public function __construct()
-    {
-        Configure::checkTokenKey();
-        $this->Discord = new Discord(Configure::read('Discord'));
+	/**
+	 * Initialize the Bot and and the ModuleManager.
+	 */
+	public function __construct()
+	{
+		Configure::checkTokenKey();
+		$this->Discord = new Discord(Configure::read('Discord'));
 
-        //Initialize the ModuleManager.
-        $modulesPriorities = [];
-        if (Configure::check('Modules.priority')) {
-            $modulesPriorities = Configure::read('Modules.priority');
-        }
+		//Initialize the ModuleManager.
+		$modulesPriorities = [];
+		if (Configure::check('Modules.priority')) {
+			$modulesPriorities = Configure::read('Modules.priority');
+		}
 
-        $this->ModuleManager = new ModuleManager($modulesPriorities);
-    }
+		$this->ModuleManager = new ModuleManager($modulesPriorities);
+	}
 
-    /**
-     * Handle the events.
-     *
-     * @return void
-     */
-    public function listen()
-    {
-        $this->Discord->on('ready', function ($discord) {
-            $discord->on('message', function ($message) {
-                if ($this->Discord->id === $message->author->id) {
-                    return;
-                }
+	/**
+	 * Handle the events.
+	 *
+	 * @return void
+	 */
+	public function listen()
+	{
+		$this->Discord->on('ready', function ($discord) {
+			$discord->on('message', function ($message) {
+				if ($this->Discord->id === $message->author->id) {
+					return;
+				}
 
-                $content = Message::parse($message->content);
-                $wrapper = Wrapper::getInstance()->setInstances($message, $this->ModuleManager);
+				$content = Message::parse($message->content);
+				$wrapper = Wrapper::getInstance()->setInstances($message, $this->ModuleManager);
 
-                //Handle the type of the message.
-                //Note : The order is very important !
-                if ($wrapper->Channel->is_private === true) {
-                    $this->ModuleManager->onPrivateMessage($wrapper, $content);
-                } elseif ($content['commandCode'] === Configure::read('Command.prefix') &&
-                            isset(Configure::read('Commands')[$content['command']])) {
-                    $command = Configure::read('Commands')[$content['command']];
+				//Handle the type of the message.
+				//Note : The order is very important !
+				if ($wrapper->Channel->is_private === true) {
+					$this->ModuleManager->onPrivateMessage($wrapper, $content);
+				} elseif ($content['commandCode'] === Configure::read('Command.prefix') &&
+							isset(Configure::read('Commands')[$content['command']])) {
+					$command = Configure::read('Commands')[$content['command']];
 
-                    if ((isset($command['admin']) && $command['admin'] === true) &&
-                            !User::hasPermission($wrapper->Message->author->id, Configure::read('Bot.admins'))) {
-                        $wrapper->Message->reply('You are not administrator of the bot.');
+					if ((isset($command['admin']) && $command['admin'] === true) &&
+							!User::hasPermission($wrapper->Message->author->id, Configure::read('Bot.admins'))) {
+						$wrapper->Message->reply('You are not administrator of the bot.');
 
-                        return;
-                    }
+						return;
+					}
 
-                    if (count($content['arguments']) < $command['params']) {
-                        $wrapper->Message->reply(Command::syntax($content));
+					if (count($content['arguments']) < $command['params']) {
+						$wrapper->Message->reply(Command::syntax($content));
 
-                        return;
-                    }
+						return;
+					}
 
-                    $this->ModuleManager->onCommandMessage($wrapper, $content);
-                } else {
-                    $this->ModuleManager->onChannelMessage($wrapper, $content);
-                }
-            });
-        });
-    }
+					$this->ModuleManager->onCommandMessage($wrapper, $content);
+				} else {
+					$this->ModuleManager->onChannelMessage($wrapper, $content);
+				}
+			});
+		});
+	}
 
-    /**
-     * Run the bot.
-     *
-     * @return void
-     */
-    public function startup()
-    {
-        $this->listen();
+	/**
+	 * Run the bot.
+	 *
+	 * @return void
+	 */
+	public function startup()
+	{
+		$this->listen();
 
-        $this->Discord->run();
-    }
+		$this->Discord->run();
+	}
 }
